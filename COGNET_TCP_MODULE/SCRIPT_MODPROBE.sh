1529ef9577c75eaaa704114889b7ffc5b50acb1e
Matteo Danieletto <matteo.danieletto@gmail.com>
2014-05-19 14:51:40 +0200

    ADDED COGNET TCP MODULE

---
+++ COGNET_TCP_MODULE/SCRIPT_MODPROBE.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+#Cognetive Network programm 
+#Copyright (C) 2014  Matteo Danieletto matteo.danieletto@dei.unipd.it
+#University of Padova, Italy +34 049 827 7778
+#This program is free software: you can redistribute it and/or modify
+#it under the terms of the GNU General Public License as published by
+#the Free Software Foundation, either version 3 of the License, or
+#(at your option) any later version.
+
+#This program is distributed in the hope that it will be useful,
+#but WITHOUT ANY WARRANTY; without even the implied warranty of
+#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#GNU General Public License for more details.
+
+#You should have received a copy of the GNU General Public License
+#along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
+#$2 0 or 1 $1=DURIP or CUBIC
+insmod ./durip.ko checkPrint=$2
+TCC=net.ipv4.tcp_congestion_control
+sysctl $TCC
+sysctl -w $TCC=$1
