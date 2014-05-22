5201e23f83fa3c2aad75a9a8bf075f1bfe1b4c22
Matteo Danieletto <matteo.danieletto@gmail.com>
2014-05-22 17:20:19 +0200

    update

--- TESTBED_SCRIPTS/scriptOLSRD_OUTPUT.sh
+++ TESTBED_SCRIPTS/scriptOLSRD_OUTPUT.sh
@@ -2,7 +2,7 @@
 
 
 while true; do 
-wget http://127.0.0.1:8081/all -qO - >> $1
-echo "####" >> fileOutput 
+wget http://127.0.0.1:8081/all -qO - >> /mnt/local/log/OLSRD/$1
+echo "####" >> /mnt/local/log/OLSRD/$1
 sleep $2; 
 done
