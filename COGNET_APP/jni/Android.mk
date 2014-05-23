cb9513bd41b3e903e6dbeeb7f258da5ccacc3543
Matteo Danieletto <matteo.danieletto@gmail.com>
2014-05-23 15:08:32 +0200

    modified Android.mk

--- COGNET_APP/jni/Android.mk
+++ COGNET_APP/jni/Android.mk
@@ -1,7 +1,14 @@
+
 LOCAL_PATH:=$(call my-dir)
 include $(CLEAR_VARS)
 LOCAL_MODULE    := olsrdLib
 LOCAL_SRC_FILES:=olsrdBridgeApp.c 
+LOCAL_LDLIBS := -llog
+include $(BUILD_SHARED_LIBRARY)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE    := olsrdLogLib
+LOCAL_SRC_FILES:=bridgeOlsrdLog.c
 LOCAL_LDLIBS := -llog
 include $(BUILD_SHARED_LIBRARY)
 
@@ -12,7 +19,7 @@ LOCAL_LDLIBS := -llog
 include $(BUILD_SHARED_LIBRARY)
 
 
-MY_LOCAL_PATH_NL:=../../libnl-3-android-nogit
+MY_LOCAL_PATH_NL:=../../LIB_NETLINK/libnl-3-android-nogit
 $(warning $(MY_LOCAL_PATH_NL))
 include $(CLEAR_VARS)
 LOCAL_MODULE := nl-3
@@ -35,23 +42,23 @@ include $(PREBUILT_SHARED_LIBRARY)
 
 
 include $(CLEAR_VARS)
-MY_LOCAL_SRC_DIR:=../../READ_DURIP_SERVER
-MY_LOCAL_SRC_DIR_MANAGE:=../../MANAGE_NODE_DURIP_TABLET
+MY_LOCAL_SRC_DIR:=../../COGNET_CLIENT_PARAMETERS
+MY_LOCAL_SRC_DIR_MANAGE:=../../MANAGER_NODE_COGNET_TABLET
 LOCAL_CFLAGS := -DTEST_MATTEO=1  
 #LOCAL_CFLAGS := -DTEST_MATTEO=1 -g
 
 LOCAL_MODULE    := MACReadServer
 LOCAL_SRC_FILES:=bridgeApp_MAC.c
-LOCAL_SRC_FILES+=$(MY_LOCAL_SRC_DIR)/serverREADDURIP.c\
+LOCAL_SRC_FILES+=$(MY_LOCAL_SRC_DIR)/serverCOGNET.c\
 				 $(MY_LOCAL_SRC_DIR)/libTHREADS_MAC/macObservation.c\
 				 $(MY_LOCAL_SRC_DIR)/libTHREADS_MAC/printMACvalue.c \
 				 $(MY_LOCAL_SRC_DIR)/libTHREADS_MAC/commonFunction.c \
-				 $(MY_LOCAL_SRC_DIR)/libTHREADS_MAC/duripThreadControlRead.c \
+				 $(MY_LOCAL_SRC_DIR)/libTHREADS_MAC/CognetThreadControlRead.c \
 				 $(MY_LOCAL_SRC_DIR)/libTHREADS_MAC/macChangeParams.c \
 				 $(MY_LOCAL_SRC_DIR)/libTHREADS_MAC/tcpObservation.c \
 				 $(MY_LOCAL_SRC_DIR)/libTHREADS_MAC/macThreadComm.c \
 				 $(MY_LOCAL_SRC_DIR)/libTHREADS_MAC/get_station.c \
-				 $(MY_LOCAL_SRC_DIR_MANAGE)/manageNodeDurip.c 
+				 $(MY_LOCAL_SRC_DIR_MANAGE)/managerNodeCognet.c 
 				 
 LOCAL_C_INCLUDES:=$(MY_LOCAL_SRC_DIR)/include
 LOCAL_C_INCLUDES+=$(MY_LOCAL_PATH_NL)/include/ \
@@ -65,31 +72,3 @@ LOCAL_LDLIBS := -llog
 include $(BUILD_SHARED_LIBRARY)
 
 
-include $(CLEAR_VARS)
-MY_LOCAL_SRC_DIR:=../../READ_DURIP_SERVER
-MY_LOCAL_SRC_DIR_MANAGE:=../../MANAGE_NODE_DURIP_TABLET
-LOCAL_CFLAGS := -DATH9K_HTC=1  
-
-LOCAL_MODULE    := ExperimentManager
-LOCAL_SRC_FILES:=experimentManager.c
-LOCAL_SRC_FILES+=$(MY_LOCAL_SRC_DIR)/serverREADDURIP.c\
-				 $(MY_LOCAL_SRC_DIR)/libTHREADS_MAC/macObservation.c\
-				 $(MY_LOCAL_SRC_DIR)/libTHREADS_MAC/printMACvalue.c \
-				 $(MY_LOCAL_SRC_DIR)/libTHREADS_MAC/commonFunction.c \
-				 $(MY_LOCAL_SRC_DIR)/libTHREADS_MAC/duripThreadControlRead.c \
-				 $(MY_LOCAL_SRC_DIR)/libTHREADS_MAC/macChangeParams.c \
-				 $(MY_LOCAL_SRC_DIR)/libTHREADS_MAC/tcpObservation.c \
-				 $(MY_LOCAL_SRC_DIR)/libTHREADS_MAC/macThreadComm.c \
-				 $(MY_LOCAL_SRC_DIR)/libTHREADS_MAC/get_station.c \
-				 $(MY_LOCAL_SRC_DIR_MANAGE)/manageNodeDurip.c 
-				 
-LOCAL_C_INCLUDES:=$(MY_LOCAL_SRC_DIR)/include
-LOCAL_C_INCLUDES+=$(MY_LOCAL_PATH_NL)/include/ \
-		  $(MY_LOCAL_PATH_NL)/android_toolchain/jni/missing_include \
-		  $(MY_LOCAL_PATH_NL)/android_toolchain/jni/generated_include 
-
-#$(warning $(LOCAL_C_INCLUDES))
-LOCAL_SHARED_LIBRARIES:=nl-3 nl-genl-3				 
-				 
-LOCAL_LDLIBS := -llog
-include $(BUILD_SHARED_LIBRARY)
